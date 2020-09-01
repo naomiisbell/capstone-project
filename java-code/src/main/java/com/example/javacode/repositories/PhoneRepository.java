f2e12458d9bc6c05231823e56ecc92833c6d4466
Naomi Isbell <naomiisbell@gmail.com>
2020-09-01 15:19:14 -0400

    Phone repo complete code

---
+++ java-code/src/main/java/com/example/javacode/repositories/PhoneRepository.java
@@ -0,0 +1,10 @@
+package com.example.javacode.repositories;
+
+import com.example.javacode.models.Phone;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhoneRepository extends CrudRepository<Phone, Long> {
+    @Query("FROM Phone u WHERE u.name = ?1 and u.price = ?2 and u.image = ?3")
+    Phone purchasePhone(String name);
+}
