526414fdcab04d1a3527fc8c0f4da8c271f9cdd8
Naomi Isbell <naomiisbell@gmail.com>
2020-09-03 15:03:29 -0400

    Delete method method

--- java-code/src/main/java/com/example/javacode/controller/PhoneController.java
+++ java-code/src/main/java/com/example/javacode/controller/PhoneController.java
@@ -3,6 +3,7 @@ package com.example.javacode.controller;
 import com.example.javacode.models.Phone;
 import com.example.javacode.service.PhoneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,5 +27,10 @@ public class PhoneController {
     @GetMapping("/purchasePhone/{name}")
     public Phone purchasePhone(@PathVariable String nameParam) {
         return phoneService.purchasePhone(nameParam);
+    }
+
+    @DeleteMapping("/{phoneId}")
+    public HttpStatus deletePhone(@PathVariable Long phoneId) {
+        return phoneService.deletePhone(phoneId);
     }
 }
