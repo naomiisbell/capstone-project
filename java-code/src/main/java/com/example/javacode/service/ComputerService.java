df77da967a584e3e97c977f6b45d15fa4b74c4ea
Naomi Isbell <naomiisbell@gmail.com>
2020-09-03 15:07:08 -0400

    Delete mapping method

--- java-code/src/main/java/com/example/javacode/service/ComputerService.java
+++ java-code/src/main/java/com/example/javacode/service/ComputerService.java
@@ -1,9 +1,11 @@
 package com.example.javacode.service;
 
 import com.example.javacode.models.Computer;
+import org.springframework.http.HttpStatus;
 
 public interface ComputerService {
     Iterable<Computer> listComputers();
     Computer purchaseComputer(String name);
     Computer createComputer(Computer computer);
+    HttpStatus deleteComputer(Long computerId);
 }
