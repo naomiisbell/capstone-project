1badb175d6d67e57e946e58f05de9b73a30d820f
Naomi Isbell <naomiisbell@gmail.com>
2020-09-03 15:08:55 -0400

    Delete function

--- java-code/src/main/java/com/example/javacode/service/PhoneService.java
+++ java-code/src/main/java/com/example/javacode/service/PhoneService.java
@@ -1,9 +1,11 @@
 package com.example.javacode.service;
 
 import com.example.javacode.models.Phone;
+import org.springframework.http.HttpStatus;
 
 public interface PhoneService {
     Iterable<Phone> listPhones();
     Phone purchasePhone(String name);
     Phone createPhone(Phone phone);
+    HttpStatus deletePhone(Long phoneId);
 }
