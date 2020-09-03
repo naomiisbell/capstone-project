1a0ce8803c41537399b9bee9d41bdc0576c7e8dc
Naomi Isbell <naomiisbell@gmail.com>
2020-09-03 15:10:06 -0400

    Delete function

--- java-code/src/main/java/com/example/javacode/service/TelevisionService.java
+++ java-code/src/main/java/com/example/javacode/service/TelevisionService.java
@@ -1,9 +1,11 @@
 package com.example.javacode.service;
 
 import com.example.javacode.models.Television;
+import org.springframework.http.HttpStatus;
 
 public interface TelevisionService {
     Iterable<Television> listTelevisions();
     Television purchaseTelevision(String name);
     Television createTelevision(Television television);
+    HttpStatus deleteTelevision(Long televisionId);
 }
