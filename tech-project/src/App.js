03d62de33a2790b29fd18227de957d64fafceca4
Naomi Isbell <naomiisbell@gmail.com>
2020-09-03 15:01:11 -0400

    Phone component render

--- tech-project/src/App.js
+++ tech-project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
+import Phones from './components/Phones';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
+      <Phones />
     </div>
   );
 }
