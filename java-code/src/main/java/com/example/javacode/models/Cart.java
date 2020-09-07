2513d66c992fd9959a09996abe161a1ac3543da5
Naomi Isbell <naomiisbell@gmail.com>
2020-09-07 15:10:38 -0400

    Cart code

---
+++ java-code/src/main/java/com/example/javacode/models/Cart.java
@@ -0,0 +1,46 @@
+package com.example.javacode.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+
+public class Cart {
+
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column
+    private Double price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
