7dd79db2856c53e0146cc5b38df615bab09fb85c
Naomi Isbell <naomiisbell@gmail.com>
2020-09-08 01:09:50 -0400

    changed price to double

--- java-code/src/main/java/com/example/javacode/models/Computer.java
+++ java-code/src/main/java/com/example/javacode/models/Computer.java
@@ -15,7 +15,7 @@ public class Computer {
     public String name;
 
     @Column
-    public String price;
+    public Double price;
 
     public Computer() {}
 
@@ -35,11 +35,11 @@ public class Computer {
         this.name = name;
     }
 
-    public String getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
