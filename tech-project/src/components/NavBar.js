23a4f2c33c2320aeccc2e4a0a82f8ff7a471bac3
Naomi Isbell <naomiisbell@gmail.com>
2020-09-08 01:05:25 -0400

    fixed div issue

--- tech-project/src/components/NavBar.js
+++ tech-project/src/components/NavBar.js
@@ -1,53 +1,57 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Phones from "./Phones";
 import Televisions from "./Televisions";
 import Computers from "./Computers";
 import Home from "./Home";
+import Cart from "./Cart";
+import "./nav.css";
 
 function NavBar() {
-    return (
+  return (
     <Router>
-        <div>
+      <div className="nav__bar">
+        <h1 className="header">Tech Junkies</h1>
         <nav>
-            <ul>
+          <ul>
             <li>
-                <Link to="/home">Home</Link>
+              <Link to="/home">Home</Link>
             </li>
             <li>
-                <Link to="/phones">Phones</Link>
+              <Link to="/phones">Phones</Link>
             </li>
             <li>
-                <Link to="/televisions">Televisions</Link>
+              <Link to="/televisions">Televisions</Link>
             </li>
             <li>
-                <Link to="/computers">Computers</Link>
+              <Link to="/computers">Computers</Link>
             </li>
-            </ul>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
         </nav>
+        </div>
 
         <Switch>
-            <Route path="/home">
-                <Home />
-            </Route>
-            <Route path="/phones">
-                <Phones />
-            </Route>
-            <Route path="/televisions">
-                <Televisions />
-            </Route>
-            <Route path="/computers">
-                <Computers />
-            </Route>
+          <Route path="/home">
+              <Home />
+          </Route>
+          <Route path="/phones">
+            <Phones />
+          </Route>
+          <Route path="/televisions">
+            <Televisions />
+          </Route>
+          <Route path="/computers">
+            <Computers />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
         </Switch>
-        </div>
     </Router>
-    );
+  );
 }
 
 export default NavBar;
