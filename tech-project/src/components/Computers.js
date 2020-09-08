83ba50d1a6e6027351093c85e30bf57126321cf9
Naomi Isbell <naomiisbell@gmail.com>
2020-09-08 01:07:19 -0400

    computer component

---
+++ tech-project/src/components/Computers.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Computers extends Component {
+    render() {
+        return (
+            <div>
+                <p>I am computer page</p>
+            </div>
+        )
+    }
+}
