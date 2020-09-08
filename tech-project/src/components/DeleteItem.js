50bd4d972e4f1ca7904111242eafde13b4e45b60
Naomi Isbell <naomiisbell@gmail.com>
2020-09-08 01:06:40 -0400

    delete item component

--- tech-project/src/components/DeleteItem.js
+++ tech-project/src/components/DeleteItem.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 function DeletePhone({ phoneName, phonePrice, getPhones, phoneId }) {
-    async function delPhone(id) {
-        try {
-            const res = await axios.delete('http://localhost:8080/phone/' + id);
-            console.log(res.data);
-        } catch(e) {
-            console.error(e);
-        }
-        getPhones();
+  async function delPhone(id) {
+    try {
+      const res = await axios.delete("http://localhost:8080/phone/" + id);
+      console.log(res.data);
+    } catch (e) {
+      console.error(e);
     }
+    getPhones();
+  }
 
-    return(
-        <div>
-            <p>{ JSON.stringify(phoneName) } : { phonePrice }</p>
-            <button id={ phoneId } onClick={ (e) => delPhone(e.target.id) }>Remove Item</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>
+        {JSON.stringify(phoneName)} : {phonePrice}
+      </p>
+      <button id={phoneId} onClick={(e) => delPhone(e.target.id)}>
+        Remove Item
+      </button>
+    </div>
+  );
 }
 
 export default DeletePhone;
