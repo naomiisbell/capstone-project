28dd55124c43637112f9fdee5d96583d2654adcc
Naomi Isbell <naomiisbell@gmail.com>
2020-09-08 20:37:52 -0400

    styling for cart and trying to add total button

--- tech-project/src/components/Cart.js
+++ tech-project/src/components/Cart.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DeleteItem from "./DeleteItem";
+import "./component.css";
+import Button from "@material-ui/core/Button";
 
 function Cart() {
   const [items, setItems] = useState(null);
 
   async function grabAllItems() {
     try {
-      const res = await axios.get("https://frozen-peak-86991.herokuapp.com/cart");
+      const res = await axios.get(
+        "https://frozen-peak-86991.herokuapp.com/cart"
+      );
       setItems(res.data);
 
       // const res2 = await axios.get("https://frozen-peak-86991.herokuapp.com/television");
@@ -20,22 +24,38 @@ function Cart() {
     }
   }
 
+  async function getTotal(e) {
+    e.preventDefault();
+    try {
+      const res = await axios.get("https://frozen-peak-86991.herokuapp.com/cart/cartTotal");
+      console.log(res.data);
+
+    } catch (e) {
+      console.error(e, e.message);
+    }
+  }
+
   useEffect(() => {
     grabAllItems();
   }, []);
 
   return (
     <div>
-      {items
-        ? items.map((item) => (
-            <DeleteItem
-              itemId={item.id}
-              itemName={item.name}
-              itemPrice={item.price}
-              getItems={grabAllItems}
-            />
-          ))
-        : "Empty"}
+      <div className="items">
+        {items
+          ? items.map((item) => (
+              <DeleteItem
+                itemId={item.id}
+                itemName={item.name}
+                itemPrice={item.price}
+                getItems={grabAllItems}
+              />
+            ))
+          : "Empty"}
+      </div>
+      <div className="totalBtn">
+        <Button onClick={(e) => getTotal(e)}>Total</Button>
+      </div>
     </div>
   );
 }
